6fa7fec8ce9d939d5697a975c7c278bcd3b3ff49
Lara Keen <lara.jr.keen@gmail.com>
2022-10-27 11:56:15 +0200

    optimization

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # collatz_conjecture
 In the range of 1 to 500, 327 has the longest sequence with 144 elements.
 In the range of 1 to 500_000, 410011 has the longest sequence with 449 elements.
-The runtime of the program is 3.927814 without optimization.
+The runtime of the program is now 2.3 seconds with optimization.
--- collatz_conjecture.rb
+++ collatz_conjecture.rb
@@ -1,17 +1,24 @@
-def collatz(n)
-  sequence = []
-  while n != 1
-    sequence << n
+require 'benchmark'
+
+def collatz(num, cache = nil)
+  sequence = [num]
+  while sequence.last != 1
+    n = sequence.last
+    if cache && cache.key?(n)
+      sequence += cache[n][1..-1]
+      break
+    end
     n = n.even? ? n / 2 : n * 3 + 1
+    sequence << n
   end
-  sequence.push(1)
+  cache[num] = sequence if cache
+  sequence
 end
 
-def longest_collatz(max)
+def longest_collatz(max, cache = nil)
   current_max_length = 0
-  num = 1
   for num in 1..max
-    if collatz(num).length > current_max_length
+    if collatz(num, cache).length > current_max_length
       current_max_length = collatz(num).length
       value = num
     end
@@ -20,9 +27,17 @@ def longest_collatz(max)
   puts "The number #{value} has the longest sequence of #{current_max_length} elements."
 end
 
-start = Time.now
-longest_collatz(500)
-longest_collatz(500_000)
-finish = Time.now
+max = 500_000
+cache = {}
 
-p "Runtime is #{(finish - start)} seconds"
+# start = Time.now
+# longest_collatz(500)
+# longest_collatz(500_000)
+# finish = Time.now
+
+# p "Runtime is #{(finish - start)} seconds"
+
+Benchmark.bm do |x|
+  x.report('without cache:') { longest_collatz(max) }
+  x.report('   with cache:') { longest_collatz(max, cache) }
+end
